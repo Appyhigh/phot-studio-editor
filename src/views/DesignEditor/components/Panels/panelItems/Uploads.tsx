dad144b78e1335573526b9fb1ed0601d637761da
Darshan-appyhigh <darshan.shah@appyhigh.com>
2023-05-05 00:46:27 +0530

    Changed: Syntax and removed @ts-ignore

--- src/views/DesignEditor/components/Panels/panelItems/Uploads.tsx
+++ src/views/DesignEditor/components/Panels/panelItems/Uploads.tsx
@@ -42,9 +42,8 @@ export default function () {
 
     editor.objects.add(upload).then(() => {
       setSelectedImage(upload.preview)
-      const fileInfo = document.getElementById("inputFile")
-      // @ts-ignore
-      if (fileInfo.value) document.getElementById("inputFile").value = ""
+      const fileInfo: any = document.getElementById("inputFile")
+      if (fileInfo.value) fileInfo.value = ""
     })
   }
 
@@ -81,7 +80,13 @@ export default function () {
         )}
         <Scrollable>
           <Block padding={"0 1.5rem"}>
-            <input onChange={handleFileInput} type="file" id="inputFile" ref={inputFileRef} style={{ display: "none" }} />
+            <input
+              onChange={handleFileInput}
+              type="file"
+              id="inputFile"
+              ref={inputFileRef}
+              style={{ display: "none" }}
+            />
             <div
               style={{
                 marginTop: "1rem",
