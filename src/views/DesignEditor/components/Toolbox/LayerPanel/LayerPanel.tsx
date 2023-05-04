c6c49153fee0f9d50cfef081f806f63d93dd1274
Darshan-appyhigh <darshan.shah@appyhigh.com>
2023-05-04 21:31:03 +0530

    Revert "Fix: Not shoing background panel in layers stack"
    This reverts commit a4ddf82bd0b491f2b4cf32285080b4dd6bb14329.

--- src/views/DesignEditor/components/Toolbox/LayerPanel/LayerPanel.tsx
+++ src/views/DesignEditor/components/Toolbox/LayerPanel/LayerPanel.tsx
@@ -8,7 +8,6 @@ import getSelectionType from "~/utils/get-selection-type"
 import useAppContext from "~/hooks/useAppContext"
 import Toolbox from "../Toolbox"
 import Icons from "~/components/Icons"
-import { backgroundLayerType } from "~/constants/contants"
 
 interface ToolboxState {
   toolbox: string
@@ -100,7 +99,7 @@ const LayerPanel = () => {
       >
         <Block className="flex-center">
           <Block
-            className="pointer"
+          className="pointer"
             onClick={() => {
               setShowObjectTypeText(!showObjectTypeText)
             }}
@@ -132,7 +131,6 @@ const LayerPanel = () => {
                 <Box />
               ) : (
                 layerObjects
-                  .filter((el) => el.metadata?.type !== backgroundLayerType)
                   .sort((a, b) => b.metadata?.generationDate - a.metadata?.generationDate)
                   .map((object) => {
                     return (
