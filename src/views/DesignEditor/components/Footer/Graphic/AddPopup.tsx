13ee27db10f91913a749d051093e997437c6618f
Ankita-appyhigh <ankita.pal@appyhigh.com>
2023-05-09 10:23:19 +0530

    removed extra option from add popup

--- src/views/DesignEditor/components/Footer/Graphic/AddPopup.tsx
+++ src/views/DesignEditor/components/Footer/Graphic/AddPopup.tsx
@@ -26,9 +26,7 @@ const AddPopup = () => {
   return (
     <Block className="addPopup">
       <Block className="addPopupCon">
-        <Box
-          className="d-flex justify-flex-start align-items-start p-absolute flex-column mt-4"
-        >
+        <Box className="d-flex justify-flex-start align-items-start p-absolute flex-column mt-4">
           <div className="p-absolute" style={{ rotate: "-90deg", top: "-60px", left: "70px" }}>
             <Icons.SliderBtn size={106} width="10" />
           </div>
@@ -58,17 +56,6 @@ const AddPopup = () => {
           <Block style={{ padding: "20px 10px 16px 24px" }}>
             <ParagraphSmall>
               <span className="pb-1">Text</span>
-            </ParagraphSmall>
-            <LabelXSmall>
-              <span className={css({ color: theme.colors.borderAccent })}>
-                Lorem ipsum dolor sit amet consecte. Pulvinar vitae sit
-              </span>
-            </LabelXSmall>
-          </Block>
-          <HorizontalLine />
-          <Block style={{ padding: "20px 10px 16px 24px" }}>
-            <ParagraphSmall>
-              <span className="pb-1">Background</span>
             </ParagraphSmall>
             <LabelXSmall>
               <span className={css({ color: theme.colors.borderAccent })}>
