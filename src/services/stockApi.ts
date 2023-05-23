2948b3a0dd069ef67d9abf72f82e8ca95eab606b
Aryan <aryan.khubchandani@gmail.com>
2023-05-23 17:50:46 +1000

    chore: update api

--- src/services/stockApi.ts
+++ src/services/stockApi.ts
@@ -3,9 +3,7 @@ import axios from "axios"
 export const getStockImages = (): Promise<any> => {
   return new Promise(async (resolve, reject) => {
     try {
-      const { data } = await axios.get(
-        "https://qaapi.phot.ai/studio/api/search?q=${q}&order=${order}&category=${category}"
-      )
+      const { data } = await axios.get("https://devapi.phot.ai/studio/api/search?q=haute&page=2&perPage=12")
       resolve(data)
     } catch (err) {
       reject(err)
