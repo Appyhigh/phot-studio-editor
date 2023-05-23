29eddde250bb4a025f6fc663e8fdd258253c12f6
Ankita-appyhigh <ankita.pal@appyhigh.com>
2023-05-23 17:30:33 +0530

    scrollbar issue fixed

--- src/components/Scrollable/Scrollable.tsx
+++ src/components/Scrollable/Scrollable.tsx
@@ -4,8 +4,8 @@ import React from "react"
 export default function ({ children, autoHide }: { children: React.ReactNode; autoHide?: boolean }) {
   return (
     <div style={{ flex: 1, position: "relative" }}>
-      <div style={{ height: "100%", width: "100%", position: "absolute", overflow: "hidden" }}>
-        <Scrollbars autoHide={autoHide}>{children}</Scrollbars>
+      <div style={{ height: "100%", width: "100%", position: "absolute",top:0, overflow: "hidden" }}>
+        <Scrollbars   autoHide={autoHide}>{children}</Scrollbars>
       </div>
     </div>
   )
