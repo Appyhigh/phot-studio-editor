44130ba8e6696c732bd1f4d59cfde4d0fe7c9317
Ankita-appyhigh <ankita.pal@appyhigh.com>
2023-05-24 11:42:40 +0530

    fixed the context-menu issues

--- src/components/Icons/Images.tsx
+++ src/components/Icons/Images.tsx
@@ -1,7 +1,7 @@
 function Images({ size }: { size: number }) {
   return (
     <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g clip-path="url(#clip0_4700_114761)">
+      <g clipPath="url(#clip0_4700_114761)">
         <path
           d="M14.8889 2H3.61111C2.72132 2 2 2.72132 2 3.61111V14.8889C2 15.7787 2.72132 16.5 3.61111 16.5H14.8889C15.7787 16.5 16.5 15.7787 16.5 14.8889V3.61111C16.5 2.72132 15.7787 2 14.8889 2Z"
           fill="white"
@@ -17,9 +17,9 @@ function Images({ size }: { size: number }) {
         <path
           d="M16.25 13.75L11.75 9.25L3.5 18.5"
           stroke="url(#paint1_linear_4700_114761)"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
       <defs>
@@ -31,8 +31,8 @@ function Images({ size }: { size: number }) {
           y2="5.04054"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#3E5387" />
-          <stop offset="1" stop-color="#542670" />
+          <stop stopColor="#3E5387" />
+          <stop offset="1" stopColor="#542670" />
         </linearGradient>
         <linearGradient
           id="paint1_linear_4700_114761"
@@ -42,8 +42,8 @@ function Images({ size }: { size: number }) {
           y2="11.795"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#3E5387" />
-          <stop offset="1" stop-color="#542670" />
+          <stop stopColor="#3E5387" />
+          <stop offset="1" stopColor="#542670" />
         </linearGradient>
         <clipPath id="clip0_4700_114761">
           <rect width="18" height="18" fill="white" />
