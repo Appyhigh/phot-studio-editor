c8d2a08296ec52275d16c5a0df9ed55c66cf5149
Ankita-appyhigh <ankita.pal@appyhigh.com>
2023-06-01 13:38:00 +0530

    tg-877 bg-option stick to top

--- src/views/DesignEditor/components/Panels/panelItems/ImagesPanel/Images.tsx
+++ src/views/DesignEditor/components/Panels/panelItems/ImagesPanel/Images.tsx
@@ -79,9 +79,7 @@ const Images = () => {
             </Block>
           </Scrollable>
         ) : (
-          <Scrollable>
             <StockImages imageAs="foreground" />
-          </Scrollable>
         )}
       </>
       <Loader isOpen={loaderPopup} />
