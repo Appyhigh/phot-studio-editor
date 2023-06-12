aaee36db94207db24f99a746aaab35be2ee87a4c
Ankita-appyhigh <ankita.pal@appyhigh.com>
2023-06-12 16:56:39 +0530

    img attribute added

--- src/utils/imagineAiController.ts
+++ src/utils/imagineAiController.ts
@@ -4,19 +4,19 @@ import { COOKIE_KEYS } from "~/utils/enum"
 
 const imagineAiController = async (
   prompt: string,
-  // image: string,
   guidance_scale: number,
   image_strength: number,
   negative_prompt: string,
   num_outputs: number,
   aspect_ratio: string,
-  style: any[]
+  style: any[],
+  image?: string
 ) => {
   try {
     const url = "https://devapi.phot.ai/v1/create-art"
     const body = {
       prompt: prompt,
-      // image: image,
+      image: image,
       guidance_scale: guidance_scale,
       image_strength: image_strength,
       negative_prompt: negative_prompt,
@@ -25,7 +25,7 @@ const imagineAiController = async (
       studio_options: {
         style: style,
       },
-    }
+    }    
     const config = {
       headers: { Authorization: `Bearer ${getCookie(COOKIE_KEYS.AUTH)}` },
     }
