6108df930c77b0928afe59322f85f63481241a39
jaybagall <jayram.bagal@appyhigh.com>
2023-06-23 10:27:19 +0530

    Fix ImageUpscaler rendering Sample Image

--- src/services/imgUpscalerSample.ts
+++ src/services/imgUpscalerSample.ts
@@ -3,10 +3,10 @@ import axios from "axios"
 export const imgUpscalerSample = async () => {
   try {
     const url = "https://devapi.phot.ai/app/api/v1/fetch-tools-assets?activity=UPSCALER"
-    const { data } = await axios.get(url)
-    return data.data[0].images
+    const { data } = await axios.get(url)    
+    return data.data
   } catch (err) {
-    console.error("An error occurred while sending the request:", error)
+    console.error("An error occurred while sending the request:", err)
     throw err
   }
 }
