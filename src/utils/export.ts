3b0c845ab1a017e8fd401eb79c34662725a57dc8
Darshan-appyhigh <darshan.shah@appyhigh.com>
2023-06-24 19:43:01 +0530

    Added: PDF Features for both single layer and whole canvas download

--- src/utils/export.ts
+++ src/utils/export.ts
@@ -1,76 +1,76 @@
 import { resizeBase64Image } from "./updateLayerBackground"
-import { PDFDocument,PageSizes } from "pdf-lib";
+import { PDFDocument, PageSizes } from "pdf-lib"
 
-export const makeDownloadToPDF = async (data: any, height?: number, width?: number) => {
+export const makeDownloadToPDF = async (data: any, height: number, width: number) => {
   // let image = data
   // if (height && width) {
   //   image = await resizeBase64Image(data, width, height)
   // }
 
   const b64 = data.substr(22)
-    try {
-      const binaryString = window.atob(b64);
-  
-      const pdfDoc = await PDFDocument.create();
-      const imageBytes = Uint8Array.from([...binaryString].map((char) => char.charCodeAt(0)));
-      const image = await pdfDoc.embedPng(imageBytes);
+  try {
+    const binaryString = window.atob(b64)
 
-        // Calculate the aspect ratio of the image
-        const aspectRatio = image.width / image.height;
+    const pdfDoc = await PDFDocument.create()
+    const imageBytes = Uint8Array.from([...binaryString].map((char) => char.charCodeAt(0)))
+    const image = await pdfDoc.embedPng(imageBytes)
 
-        // Set the page dimensions to A4 size (595.28 x 841.89)
-        const page = pdfDoc.addPage(PageSizes.A4);
-    
-        let scaledWidth, scaledHeight;
-    
-        if (width && height) {
-          // Both width and height provided
-          scaledWidth = width;
-          scaledHeight = height;
-        } else if (width) {
-          // Only width provided, calculate height based on aspect ratio
-          scaledWidth = width;
-          scaledHeight = width / aspectRatio;
-        } else if (height) {
-          // Only height provided, calculate width based on aspect ratio
-          scaledHeight = height;
-          scaledWidth = height * aspectRatio;
-        } else {
-          // No width or height provided, use image's original dimensions
-          scaledWidth = image.width;
-          scaledHeight = image.height;
-        }
-    
-        // Calculate the horizontal and vertical positions to center the image
-        const horizontalPosition = (page.getWidth() - scaledWidth) / 2;
-        const verticalPosition = (page.getHeight() - scaledHeight) / 2;
-    
-        page.drawImage(image, {
-          x: horizontalPosition,
-          y: verticalPosition,
-          width: scaledWidth,
-          height: scaledHeight,
-        });
-  
-      const pdfBytes = await pdfDoc.save();
-  
-      const pdfBlob = new Blob([pdfBytes], { type: 'application/pdf' });
-  
-      // Create a download link
-      const downloadLink = document.createElement('a');
-      downloadLink.href = URL.createObjectURL(pdfBlob);
-      downloadLink.download = 'converted.pdf';
-  
-      // Simulate click to trigger the download
-      document.body.appendChild(downloadLink);
-      downloadLink.click();
-  
-      // Cleanup
-      URL.revokeObjectURL(downloadLink.href);
-      document.body.removeChild(downloadLink);
-    } catch (error) {
-      console.error('Error converting image to PDF:', error);
-    }
+    // Calculate the aspect ratio of the image
+    // const aspectRatio = image.width / image.height
+
+    // Set the page dimensions to A4 size (595.28 x 841.89)
+    const page = pdfDoc.addPage([width, height])
+
+    // let scaledWidth, scaledHeight
+
+    // if (width && height) {
+    //   // Both width and height provided
+    //   scaledWidth = width
+    //   scaledHeight = height
+    // } else if (width) {
+    //   // Only width provided, calculate height based on aspect ratio
+    //   scaledWidth = width
+    //   scaledHeight = width / aspectRatio
+    // } else if (height) {
+    //   // Only height provided, calculate width based on aspect ratio
+    //   scaledHeight = height
+    //   scaledWidth = height * aspectRatio
+    // } else {
+    //   // No width or height provided, use image's original dimensions
+    //   scaledWidth = image.width
+    //   scaledHeight = image.height
+    // }
+
+    // Calculate the horizontal and vertical positions to center the image
+    const horizontalPosition = (page.getWidth() - width) / 2
+    const verticalPosition = (page.getHeight() - height) / 2
+
+    page.drawImage(image, {
+      x: horizontalPosition,
+      y: verticalPosition,
+      width: width,
+      height: height,
+    })
+
+    const pdfBytes = await pdfDoc.save()
+
+    const pdfBlob = new Blob([pdfBytes], { type: "application/pdf" })
+
+    // Create a download link
+    const downloadLink = document.createElement("a")
+    downloadLink.href = URL.createObjectURL(pdfBlob)
+    downloadLink.download = "converted.pdf"
+
+    // Simulate click to trigger the download
+    document.body.appendChild(downloadLink)
+    downloadLink.click()
+
+    // Cleanup
+    URL.revokeObjectURL(downloadLink.href)
+    document.body.removeChild(downloadLink)
+  } catch (error) {
+    console.error("Error converting image to PDF:", error)
+  }
 }
 
 export const makeDownloadToPNG = async (data: any, selectedType: string, height?: number, width?: number) => {
@@ -118,4 +118,43 @@ export const toDataURL = (url: any, callback: any) => {
   xhr.open("GET", url)
   xhr.responseType = "blob"
   xhr.send()
+}
+
+export async function getBase64ImageData(imageUrl) {
+  try {
+    const response = await fetch(imageUrl)
+
+    if (!response.ok) {
+      throw new Error("Image request failed")
+    }
+
+    const blob = await response.blob()
+    const reader = new FileReader()
+
+    return new Promise((resolve, reject) => {
+      reader.onloadend = () => {
+        const img = new Image()
+        img.src = reader.result
+
+        img.onload = () => {
+          const canvas = document.createElement("canvas")
+          canvas.width = img.width
+          canvas.height = img.height
+
+          const ctx = canvas.getContext("2d")
+          ctx.drawImage(img, 0, 0)
+
+          const base64Data = canvas.toDataURL("image/png")
+          resolve(base64Data)
+        }
+      }
+
+      reader.onerror = reject
+
+      reader.readAsDataURL(blob)
+    })
+  } catch (error) {
+    console.error("Error:", error)
+    return null
+  }
 }
--- src/views/DesignEditor/components/Footer/Graphic/DownloadPopup/DownloadPopup.tsx
+++ src/views/DesignEditor/components/Footer/Graphic/DownloadPopup/DownloadPopup.tsx
@@ -5,7 +5,13 @@ import SelectInput from "~/components/UI/Common/SelectInput/SelectInput"
 import SliderBar from "~/components/UI/Common/SliderBar"
 import classes from "./style.module.css"
 import clsx from "clsx"
-import { makeDownloadToPNG, makeDownloadToSVGHandler, makeDownloadToPDF } from "~/utils/export"
+import {
+  makeDownloadToPNG,
+  makeDownloadToSVGHandler,
+  makeDownloadToPDF,
+  toDataURL,
+  getBase64ImageData,
+} from "~/utils/export"
 import { backgroundLayerType } from "~/constants/contants"
 
 const DownloadPopup = ({ typeOfDownload, typeGroup }: any) => {
@@ -60,16 +66,20 @@ const DownloadPopup = ({ typeOfDownload, typeGroup }: any) => {
         }
         image = (await editor.renderer.render(template)) as string
       }
+
       const nWidth = activeObject.width * activeObject?.scaleX * sizeVal
       const nHeight = activeObject.height * activeObject?.scaleY * sizeVal
 
-      if (selectedType != "svg" && selectedType !== "Pdf") {
-        makeDownloadToPNG(image, selectedType, nHeight, nWidth)
+      if (!image.startsWith("data")) {
+        // @ts-ignore
+        image = await getBase64ImageData(image)
+      }
+
+      if (selectedType === "svg") {
+        makeDownloadToSVG(image, { width: nWidth, height: nHeight })
       } else if (selectedType === "Pdf") {
         makeDownloadToPDF(image, nWidth, nHeight)
-      } else {
-        makeDownloadToSVG(image, { width: nWidth, height: nHeight })
-      }
+      } else makeDownloadToPNG(image, selectedType, nHeight, nWidth)
     }
   }, [editor, selectedType, activeObject, objects, sizeVal])
 
@@ -106,11 +116,11 @@ const DownloadPopup = ({ typeOfDownload, typeGroup }: any) => {
       const image = (await editor.renderer.render(template)) as string
       const nWidth = frame.width * sizeVal
       const nHeight = frame.height * sizeVal
-      if (selectedType != "svg" && selectedType != "Pdf") {
-        makeDownloadToPNG(image, selectedType, nHeight, nWidth)
+      if (selectedType === "svg") {
+        makeDownloadToSVG(image, { width: nWidth, height: nHeight })
       } else if (selectedType === "Pdf") {
         makeDownloadToPDF(image, nWidth, nHeight)
-      } else makeDownloadToSVG(image, { width: nWidth, height: nHeight })
+      } else makeDownloadToPNG(image, selectedType, nHeight, nWidth)
     }
   }, [editor, selectedType, frame, objects, sizeVal])
 
@@ -138,31 +148,33 @@ const DownloadPopup = ({ typeOfDownload, typeGroup }: any) => {
             <div className={clsx("pb-1", classes.subHeading)}>File Type</div>
             <SelectInput handleChange={handleTypeChange} selectedType={selectedType} typeOfDownload={typeOfDownload} />
           </Block>
-          <Block className="mb-1">
-            <div className={clsx("mb-0", classes.subHeading)}>Size</div>
-            <SliderBar
-              step={0.05}
-              width={typeOfDownload === "single-layer" ? "210px" : "424px"}
-              minVal={minSize}
-              maxVal={maxSize}
-              thumbSize={"14px"}
-              val={[sizeVal]}
-              handleChange={handleSizeChange}
-            />
-            <div className={classes.subHeading}>
-              <span>
-                {" "}
-                {typeOfDownload === "single-layer"
-                  ? `${(activeObject?.width * activeObject?.scaleX * sizeVal).toFixed(0)} * ${(
-                      activeObject?.height *
-                      activeObject?.scaleY *
-                      sizeVal
-                    ).toFixed(0)}`
-                  : `${frame?.width * sizeVal} * ${frame?.height * sizeVal}`}
-                px
-              </span>
-            </div>
-          </Block>
+          {selectedType !== "Pdf" && (
+            <Block className="mb-1">
+              <div className={clsx("mb-0", classes.subHeading)}>Size</div>
+              <SliderBar
+                step={0.05}
+                width={typeOfDownload === "single-layer" ? "210px" : "424px"}
+                minVal={minSize}
+                maxVal={maxSize}
+                thumbSize={"14px"}
+                val={[sizeVal]}
+                handleChange={handleSizeChange}
+              />
+              <div className={classes.subHeading}>
+                <span>
+                  {" "}
+                  {typeOfDownload === "single-layer"
+                    ? `${(activeObject?.width * activeObject?.scaleX * sizeVal).toFixed(0)} * ${(
+                        activeObject?.height *
+                        activeObject?.scaleY *
+                        sizeVal
+                      ).toFixed(0)}`
+                    : `${frame?.width * sizeVal} * ${frame?.height * sizeVal}`}
+                  px
+                </span>
+              </div>
+            </Block>
+          )}
           {(selectedType === "jpg" || selectedType === "jpeg") && (
             <Block className="mb-1 mt-1">
               {/* <div className={clsx("mb-0", classes.subHeading)}>Quality</div>
