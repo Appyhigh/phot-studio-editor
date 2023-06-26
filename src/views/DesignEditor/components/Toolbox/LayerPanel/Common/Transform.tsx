7ed56e1024bd49d6ebdd23225a7c95ebadcd796f
Aryan <aryan.khubchandani@gmail.com>
2023-06-27 00:22:31 +0530

    feat: provide default value

--- src/views/DesignEditor/components/Toolbox/LayerPanel/Common/Transform.tsx
+++ src/views/DesignEditor/components/Toolbox/LayerPanel/Common/Transform.tsx
@@ -1,13 +1,27 @@
 import { TransformOptions } from "~/views/DesignEditor/utils/TransformOption"
 import BaseSlider from "./BaseSlider"
 import classes from "./style.module.css"
+import { useActiveObject } from "@layerhub-io/react"
 
 const Transform = () => {
+  const activeObject: any = useActiveObject()
 
   return (
     <div className={classes.adjustState}>
       {TransformOptions.map((each, idx) => (
-        <BaseSlider  {...each} key={idx} />
+        <BaseSlider
+          {...each}
+          value={
+            each.type == "size"
+              ? activeObject.fontSize
+              : each.type == "lineHeight"
+              ? activeObject.lineHeight
+              : each.type == "letterSpacing"
+              ? activeObject.charSpacing
+              : null
+          }
+          key={idx}
+        />
       ))}
     </div>
   )
