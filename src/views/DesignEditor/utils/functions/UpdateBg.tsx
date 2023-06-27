13bfebb93e2f7f37d9a35429000bb6a932d6a2b9
Aryan <aryan.khubchandani@gmail.com>
2023-06-27 17:07:01 +0530

    refactor: remove add logic from update bg

--- src/views/DesignEditor/utils/functions/UpdateBg.tsx
+++ src/views/DesignEditor/utils/functions/UpdateBg.tsx
@@ -4,27 +4,32 @@ const UpdateBg = (imageUrl: string, editor: any, frame: any) => {
   const bgObject = editor?.frame?.background?.canvas?._objects.filter(
     (el: any) => el.metadata?.type === backgroundLayerType || el.metadata?.type === deviceUploadType
   )[0]
-  editor.frame.resize({ width: frame.width, height: frame.height })
-
-  if (bgObject) {
-    editor.frame.resize({ width: frame.width, height: frame.height })
-    editor.objects.remove(bgObject.id)
-    editor.objects.unsetBackgroundImage()
-  }
-
   editor?.frame?.setBackgroundColor("#FFF")
 
+  var imageElement = document.createElement("img")
+  imageElement.setAttribute("crossorigin", "Anonymous")
+  imageElement.setAttribute("class", "canvas-img")
+  imageElement.setAttribute("src", imageUrl)
   const options = {
     type: "BackgroundImage",
     src: imageUrl,
     preview: imageUrl,
     metadata: { generationDate: new Date().getTime(), type: deviceUploadType },
+    _element: imageElement,
+    _originalElement: imageElement,
   }
-  editor.objects.add(options).then(() => {
-    setTimeout(() => {
-      editor.objects.setAsBackgroundImage()
-    }, 100)
-  })
+
+  if (bgObject) {
+    console.log("THIS")
+    editor.objects.update(options, bgObject.id)
+  } else {
+    console.log("THAT")
+    editor.objects.add(options).then(() => {
+      setTimeout(() => {
+        editor.objects.setAsBackgroundImage()
+      }, 100)
+    })
+  }
 }
 
 export default UpdateBg
