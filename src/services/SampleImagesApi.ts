2e7ccbb2ef03b6eee3d5f839d1980091c846be91
Aryan <aryan.khubchandani@gmail.com>
2023-06-28 12:27:36 +0530

    feat: temp fix for remaining apis

--- src/services/SampleImagesApi.ts
+++ src/services/SampleImagesApi.ts
@@ -1,14 +1,13 @@
 import axios from "axios"
 
-export const SampleImagesApi = (type:any): Promise<any> => {
+export const SampleImagesApi = (type: any): Promise<any> => {
   return new Promise(async (resolve, reject) => {
     try {
-      const { data } = await axios.get(
-        `https://devapi.phot.ai/app/api/v1/fetch-tools-assets?activity=${type}`
-      )      
+      // Temporary fix to use bg remover tool to fetch sample image until new API for other tools come
+      if (type == "") type = "STUDIO_BACKGROUND_REMOVER"
+      const { data } = await axios.get(`https://devapi.phot.ai/app/api/v1/fetch-tools-assets?activity=${type}`)
       resolve(data.data)
     } catch (err) {
-
       reject(err)
     }
   })
