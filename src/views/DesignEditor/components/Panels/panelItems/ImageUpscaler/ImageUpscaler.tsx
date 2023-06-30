1ce8129843382e53adb1de2e404efca3f1eb9be9
Ankita-appyhigh <ankita.pal@appyhigh.com>
2023-06-30 17:44:32 +0530

    tg-26 fixed the loading on image-upscaler result screen

--- src/views/DesignEditor/components/Panels/panelItems/ImageUpscaler/ImageUpscaler.tsx
+++ src/views/DesignEditor/components/Panels/panelItems/ImageUpscaler/ImageUpscaler.tsx
@@ -110,7 +110,7 @@ const ImageUpscaler = () => {
       setAutoCallAPI(true)
     } else {
       setImageLoading(true)
-      setLoadingImgCt(3)
+      setLoadingImgCt(2)
       setAutoCallAPI(false)
       setCurrentActiveImg(-1)
       setImgScalerPanelInfo((prev: any) => ({ ...prev, resultSectionVisible: true }))
@@ -118,7 +118,7 @@ const ImageUpscaler = () => {
       img2Upscaler(imgScalerInfo.src)
         .then((response) => {
           setImageLoading(false)
-          setImgScalerInfo((prev: any) => ({ ...prev, result: [imgScalerInfo.src, response] }))
+          setImgScalerInfo((prev: any) => ({ ...prev, result: [response] }))
         })
         .catch((error) => {
           setImageLoading(false)
@@ -182,7 +182,7 @@ const ImageUpscaler = () => {
       setShowLoginPopup(true)
       setAutoCallAPI(true)
     } else {
-      setLoadingImgCt(3)
+      setLoadingImgCt(2)
       setCurrentActiveImg(-1)
       setAutoCallAPI(false)
       setImageLoading(true)
@@ -194,7 +194,7 @@ const ImageUpscaler = () => {
           setImageLoading(false)
           setImgScalerInfo((prev: any) => ({
             ...prev,
-            result: [imgScalerInfo.src, response["2k"].url, response["4k"].url],
+            result: [response["2k"].url, response["4k"].url],
           }))
         })
         .catch((error) => {
@@ -390,7 +390,6 @@ const ImageUpscaler = () => {
           <Block
             onClick={() => {
               setImgScalerPanelInfo((prev: any) => ({ ...prev, resultSectionVisible: false }))
-
               setImgScalerInfo((prev: any) => ({ ...prev, result: [], showclearTooltip: true }))
             }}
             $style={{ cursor: "pointer", display: "flex" }}
@@ -400,6 +399,17 @@ const ImageUpscaler = () => {
           </Block>
 
           <div className={classes.resultImages}>
+            <div className={clsx("pointer", classes.eachImg, currentActiveImg === 2 && classes.currentActiveImg)}>
+              <img
+                src={imgScalerInfo.src}
+                alt="result-img"
+                onClick={() => {
+                  addImg(imgScalerInfo.src, 2)
+                }}
+              />
+
+              <div className={classes.resultLabel}>{"Original"}</div>
+            </div>
             {imgScalerInfo.result.map((each, _idx) => {
               return (
                 <div
@@ -413,8 +423,7 @@ const ImageUpscaler = () => {
                       addImg(each, _idx)
                     }}
                   />
-
-                  <div className={classes.resultLabel}>{_idx == 0 ? "Original" : _idx == 1 ? "2x" : "4x"}</div>
+                  <div className={classes.resultLabel}>{_idx == 0 ? "2x" : "4x"}</div>
                 </div>
               )
             })}
@@ -425,9 +434,9 @@ const ImageUpscaler = () => {
                   {<img className={classes.imagesLoader} src={LoaderSpinner} />}{" "}
                 </div>
               ))}
-            {!imageLoading && imgScalerInfo.scaler === 2 && imgScalerInfo.result.length === 2 && (
+            {!imageLoading && imgScalerInfo.scaler === 2 && imgScalerInfo.result.length === 1 && (
               <div className={clsx("pointer", classes.eachImg, classes.lockedImg)}>
-                <img src={imgScalerInfo.src} alt="result-img" className={classes.blurImg} onClick={() => {}} />
+                <img src={imgScalerInfo.src} alt="result-img" className={classes.blurImg} />
 
                 <div className={clsx(classes.resultLabel)}>{"4x"}</div>
                 <div
@@ -451,19 +460,16 @@ const ImageItem = ({
   preview,
   onClick,
   imageLoading,
-  index
 }: {
   preview: any
   onClick?: (option: any) => void
   imageLoading: boolean
-  index:any
 }) => {
   return (
     <div
       onClick={onClick}
       className={clsx("pointer p-relative", classes.imageItemSection)}
       style={{ pointerEvents: imageLoading ? "none" : "auto" }}
-      key={index}
     >
       <div className={clsx("p-absolute", classes.imageItem)} />
       <img src={preview} className={classes.imagePreview} />
