60ac033302c882a3f0e836d0126a54d2044d4686
jaybagall <jayram.bagal@appyhigh.com>
2023-07-06 16:43:29 +0530

    TG-1093:Removing extra code

--- src/views/DesignEditor/components/Toolbox/Shared/TextEffects.tsx
+++ src/views/DesignEditor/components/Toolbox/Shared/TextEffects.tsx
@@ -1,24 +1,13 @@
-import { useActiveObject, useEditor } from "@layerhub-io/react"
+import { useEditor } from "@layerhub-io/react"
 import { Block } from "baseui/block"
 import { Button, SIZE, KIND } from "baseui/button"
 import { PLACEMENT, StatefulPopover } from "baseui/popover"
-import AlignCenter from "~/components/Icons/AlignCenter"
-import AlignLeft from "~/components/Icons/AlignLeft"
-import AlignRight from "~/components/Icons/AlignRight"
-import AlignTop from "~/components/Icons/AlignTop"
-import AlignMiddle from "~/components/Icons/AlignMiddle"
-import AlignBottom from "~/components/Icons/AlignBottom"
-import { ToolButton } from "./ToolButton"
-import AlignCenterIn from "~/components/Icons/AlignCenterIn"
 import { TEXT_EFFECTS } from "~/constants/design-editor"
 import { useState } from "react"
-import { throttle } from "lodash"
 
 const TextEffectsNew = ({ type }: any) => {
   const editor = useEditor()
   const [color, setColor] = useState("#b32aa9")
-  const activeObject = useActiveObject()
-
 
   const EFFECTS = {
     None: {
@@ -90,15 +79,6 @@ const TextEffectsNew = ({ type }: any) => {
     },
   }
 
-
-  
-    const updateObjectFill = throttle((color: string) => {
-      if (activeObject) {
-        editor.objects.update({ fill: color })
-      }
-  
-      setColor(color)
-    }, 100)
 
   const applyEffect = (name: string) => {
     if (editor) {
