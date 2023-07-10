9dc05a181804bd86465804497dc079ae6c00d21a
Ankita-appyhigh <ankita.pal@appyhigh.com>
2023-07-10 15:28:56 +0530

    pointer wip

--- src/views/DesignEditor/components/Panels/PanelsList/PanelsList.tsx
+++ src/views/DesignEditor/components/Panels/PanelsList/PanelsList.tsx
@@ -9,6 +9,9 @@ import Scrollable from "~/components/Scrollable"
 import { Block } from "baseui/block"
 import classes from "./style.module.css"
 import clsx from "clsx"
+import PointerIcon from "~/components/Icons/PointerIcon"
+import { OBJECT_REMOVER, OBJECT_REPLACER } from "~/constants/contants"
+import Scrollbars from "@layerhub-io/react-custom-scrollbar"
 
 const Container = styled<"div", {}, Theme>("div", ({ $theme }) => ({
   backgroundColor: $theme.colors.white,
@@ -26,19 +29,19 @@ const PanelsList = () => {
 
   return (
     <Container className={classes.panelListSection}>
-      <Scrollable autoHide={true}>
-        {PANEL_ITEMS.map((panelListItem) => (
-          <PanelListItem
-            // @ts-ignore
-            label={panelListItem.label}
-            name={panelListItem.name}
-            key={panelListItem.id}
-            // @ts-ignore
-            icon={panelListItem.icon ? panelListItem.icon : panelListItem.name}
-            activePanel={activePanel}
-          />
-        ))}
-      </Scrollable>
+        <Scrollable autoHide={true}>
+          {PANEL_ITEMS.map((panelListItem) => (
+            <PanelListItem
+              // @ts-ignore
+              label={panelListItem.label}
+              name={panelListItem.name}
+              key={panelListItem.id}
+              // @ts-ignore
+              icon={panelListItem.icon ? panelListItem.icon : panelListItem.name}
+              activePanel={activePanel}
+            />
+          ))}
+        </Scrollable>
     </Container>
   )
 }
@@ -52,24 +55,33 @@ const PanelListItem = ({ label, icon, activePanel, name }: any) => {
   return (
     <Block
       id="EditorPanelList"
-      className={clsx(classes.panelListItem, "flex-center-column")}
+      className={clsx(classes.panelListItem, "flex-center-column p-relative")}
       onClick={() => {
         setIsSidebarOpen(true)
         setActivePanel(name)
       }}
     >
       {name === "Images" ? (
-        <div className={clsx(classes.imagesIcon,activePanel !== name && classes.inActivePanel)}  >
-          <Icon size={24} color={activePanel !== name ? "#9BA6B0" :"#4E19C6"} />
+        <div className={clsx(classes.imagesIcon, activePanel !== name && classes.inActivePanel)}>
+          <Icon size={24} color={activePanel !== name ? "#9BA6B0" : "#4E19C6"} />
         </div>
       ) : (
-        <Icon size={24} color={activePanel !== name ? "#9BA6B0" :"#4E19C6"} />
+        <Icon size={24} color={activePanel !== name ? "#9BA6B0" : "#4E19C6"} />
       )}
 
       <Block
-        className={clsx("text-center", classes.panelListItemEach, activePanel === name && classes.activePanelItem)}
+        className={clsx(
+          "text-center p-relative",
+          classes.panelListItemEach,
+          activePanel === name && classes.activePanelItem
+        )}
       >
         {label}
+        {activePanel === name && (activePanel === OBJECT_REMOVER || activePanel === OBJECT_REPLACER) && (
+          <div className={classes.chevronIcon}>
+            <PointerIcon />
+          </div>
+        )}
       </Block>
     </Block>
   )
