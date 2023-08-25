93f323c65d28b9c5c9ade12ce544a3ea34b79211
jaybagall <jayram.bagal@appyhigh.com>
2023-08-25 17:30:13 +0530

    TG-168: fix Image upscaler > Sample images have stopped coming

--- src/services/SampleImagesApi.ts
+++ src/services/SampleImagesApi.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 export const SampleImagesApi = (type: any): Promise<any> => {
   return new Promise(async (resolve, reject) => {
     try {
-      // Temporary fix to use bg remover tool to fetch sample image until new API for other tools come
+      // Temporary fix to use bg remover tool to fetch sample image until new API for other tools comee
       if (type == "") type = "STUDIO_BACKGROUND_REMOVER"
       const { data } = await axios.get(`https://devapi.phot.ai/app/api/v1/fetch-tools-assets?activity=${type}`)
       resolve(data.data)
