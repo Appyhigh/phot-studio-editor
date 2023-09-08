298a0669ad927bbce29db40c5ed2a214180ca7ce
Darshan-appyhigh <darshan.shah@appyhigh.com>
2023-09-08 08:39:19 +0530

    Added: Missing canvas ratio

--- src/views/DesignEditor/utils/AspectRatio.ts
+++ src/views/DesignEditor/utils/AspectRatio.ts
@@ -3,10 +3,9 @@ export const aspectRatio = [
   { x: 2, y: 3, xWid: 8, yWid: 12 },
   { x: 3, y: 2, xWid: 18, yWid: 12 },
   { x: 2, y: 1, xWid: 12, yWid: 6 },
-  { x: 4, y: 5, xWid: 16, yWid: 20 },
+  { x: 1, y: 2, xWid: 6, yWid: 12 },
   { x: 9, y: 16, xWid: 9, yWid: 16 },
   { x: 16, y: 9, xWid: 16, yWid: 9 },
-  { x: 2, y: 1, xWid: 12, yWid: 6 },
   { x: 4, y: 5, xWid: 16, yWid: 20 },
-
+  { x: 5, y: 4, xWid: 20, yWid: 16 },
 ]
