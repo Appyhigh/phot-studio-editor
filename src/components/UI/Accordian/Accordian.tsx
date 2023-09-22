f656b72676a11273474607d02b126733d8a4a6c0
jaybagall <jayram.bagal@appyhigh.com>
2023-09-22 09:18:44 +0530

    fix some UT issues

--- src/components/UI/Accordian/Accordian.tsx
+++ src/components/UI/Accordian/Accordian.tsx
@@ -2,8 +2,12 @@ import { useState } from "react"
 import classes from "./style.module.css"
 import clsx from "clsx"
 import Icons from "~/components/Icons"
+import useAppContext from "~/hooks/useAppContext"
 
 const Accordian = ({ label, heading, isVisited, children, isOpen, isComplete, handleClick }: any) => {
+  const { activePanel } = useAppContext()
+  // @ts-ignore
+  const visited = activePanel === "ProductPhotoshoot" ? isVisited : isOpen;
   return (
     <div className={classes.accordianMain}>
       <div
@@ -20,12 +24,12 @@ const Accordian = ({ label, heading, isVisited, children, isOpen, isComplete, ha
               </div>
             </div>
           ) : (
-            <div className={classes.label} style={{ background: isVisited ? "#6729f3" : "#92929d" }}>
+            <div className={classes.label} style={{ background: visited ? "#6729f3" : "#92929d" }}>
               {label}
             </div>
           )}
 
-          <p style={{ color: isVisited ? "#44444F" : "#92929D", fontWeight: "400" }}>{heading}</p>
+          <p style={{ color: visited ? "#44444F" : "#92929D", fontWeight: "400" }}>{heading}</p>
         </div>
 
         <div className="flex-1"></div>
