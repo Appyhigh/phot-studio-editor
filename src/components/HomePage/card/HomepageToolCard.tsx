67211a25c9c816e85ac05ed30dffe8ede2e67739
jaybagall <jayram.bagal@appyhigh.com>
2023-10-18 19:16:58 +0530

    remove comming soon from productphotoshoot

--- src/components/HomePage/card/HomepageToolCard.tsx
+++ src/components/HomePage/card/HomepageToolCard.tsx
@@ -1,12 +1,11 @@
 import { Block } from "baseui/block"
 import classes from "./style.module.css"
 import { useNavigate } from "react-router-dom"
-import { Block } from "baseui/block"
-import classes from "./style.module.css"
 import Icons from "~/components/Icons"
 const HomepageToolCard = ({ data }: any) => {
+  const navigate = useNavigate()
   return (
-    <Block className={classes.toolCard}>
+    <Block className={classes.toolCard} onClick={() => navigate(data.title === "Product photoshoot" ? "/product-photoshoot" : "/home")}>
       <div className={classes.toolCardImgCon}>
         <img src={data.src} alt={data.title} />
       </div>
@@ -14,10 +13,12 @@ const HomepageToolCard = ({ data }: any) => {
         <h3 className={classes.toolCardHeader}>{data.title}</h3>
         <p className={classes.toolCardDesc}>{data.discription}</p>
       </div>
-      <div className={classes.comingSoonWrapper}>
-        {/* comming soon Icon */}
-      </div>
+      {
+        data.title !== "Product photoshoot" && <div className={classes.comingSoonWrapper}>
+          <Icons.CommingSoon />
+        </div>
+      }
     </Block>
-  );
-};
-export default HomepageToolCard;
+  )
+}
+export default HomepageToolCard
