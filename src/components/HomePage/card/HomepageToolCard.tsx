b94a4d831fd66b824b1de0a6da3e19ae06fcbc7a
Darshan-appyhigh <darshan.shah@appyhigh.com>
2023-10-18 17:50:57 +0530

    Added: Coming soon on product photoshoot card

--- src/components/HomePage/card/HomepageToolCard.tsx
+++ src/components/HomePage/card/HomepageToolCard.tsx
@@ -1,20 +1,22 @@
-import { Block } from 'baseui/block';
-import classes from './style.module.css'
-import Icons from '~/components/Icons';
+import { Block } from "baseui/block"
+import classes from "./style.module.css"
+import Icons from "~/components/Icons"
 const HomepageToolCard = ({ data }: any) => {
-    return (
-        <Block className={classes.toolCard}>
-            <div className={classes.toolCardImgCon}>
-                <img src={data.src} alt={data.title} />
-            </div>
-            <div className={classes.descCon}>
-                <h3 className={classes.toolCardHeader}>{data.title}</h3>
-                <p className={classes.toolCardDesc}>{data.discription}</p>
-            </div>
-            {data.title !== 'Product photoshoot' && <div className={classes.comingSoonWrapper}>
-                <Icons.CommingSoon />
-            </div>}
-        </Block>
-    );
-};
-export default HomepageToolCard;
+  return (
+    <Block className={classes.toolCard}>
+      <div className={classes.toolCardImgCon}>
+        <img src={data.src} alt={data.title} />
+      </div>
+      <div className={classes.descCon}>
+        <h3 className={classes.toolCardHeader}>{data.title}</h3>
+        <p className={classes.toolCardDesc}>{data.discription}</p>
+      </div>
+      {
+        <div className={classes.comingSoonWrapper}>
+          <Icons.CommingSoon />
+        </div>
+      }
+    </Block>
+  )
+}
+export default HomepageToolCard
